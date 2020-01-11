8f1fdcbb9c430af29cd80b0de181512fb3074d1b
Adrien <adrien.blanchet@live.fr>
2020-01-11 18:01:01 +0900

    Up

--- common_profile.sh
+++ common_profile.sh
@@ -255,6 +255,9 @@ function setup_programs(){
   ccenv python 3.6.7
   echo "   ├─ Python Version : $(python --version)"
   ccenv gcc 7.3.0
+  # ccenv gcc 5.5.0
+  export CC="$(which gcc)"
+  export CXX="$(which g++)"
   echo "   ├─ GCC Version : $(gcc --version | head -n 1)"
 }; export -f setup_programs
 
