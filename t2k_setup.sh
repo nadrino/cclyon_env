be87e5bc20ad8d480bb18d25b81ca719b743b515
Adrien BLANCHET <adrien.blanchet@live.fr>
2020-08-26 12:33:03 +0200

    Fast commit.

--- t2k_setup.sh
+++ t2k_setup.sh
@@ -387,9 +387,11 @@ function pull_p_theta_dev(){
 
   if [ "" != "$1" ]
   then
+    export NIWG=$REPO_DIR/NIWGReWeight
     cmake \
       -DCMAKE_INSTALL_PREFIX:PATH=$INSTALL_DIR/P-theta-dev \
       -D CMAKE_BUILD_TYPE=$1 \
+      -D USE_NIWG=TRUE \
       $REPO_DIR/P-theta-dev/Minimal/.
     make clean
   fi
