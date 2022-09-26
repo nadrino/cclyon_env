59995b8afebef4e761c088aa910897357c024f74
Adrien Blanchet <adrien.blanchet@live.fr>
2022-09-26 12:01:32 +0200

    Fast commit.

--- t2k_setup.sh
+++ t2k_setup.sh
@@ -403,7 +403,6 @@ function pull_gundam(){
       -D YAMLCPP_DIR=$COMMON_INSTALL_DIR/yaml-cpp \
       -D USE_STATIC_LINKS=ON \
       -D WITH_CACHE_MANAGER=ON \
-      -D DISABLE_CUDA=OFF \
       $REPO_DIR/gundam/.
     make clean
   fi
