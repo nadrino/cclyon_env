0ffdd5c8dbe8638ca97a85c45fb88f6daeaa610a
Adrien Blanchet <adrien.blanchet@live.fr>
2022-09-27 16:20:10 +0200

    Fast commit.

--- common_profile.sh
+++ common_profile.sh
@@ -538,6 +538,10 @@ function setPsyche(){
   source /sps/t2k/ablanche/repo/nd280/psycheMaster_3.69/bin/setup.sh
 }
 
+function printLog(){
+  tail -n +1 -f $LOGS_DIR/*/$1
+}
+
 alias nextcloud='cadaver https://nextcloud.nms.kcl.ac.uk/remote.php/dav/files/ASGReader'
 
 # Default software setup
