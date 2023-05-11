7c21952bc288f8017f0d40dc9e4dc00e2e16f164
Adrien Blanchet <adrien.blanchet@live.fr>
2023-05-11 11:17:56 +0200

    Fast commit.

--- common_profile.sh
+++ common_profile.sh
@@ -290,25 +290,27 @@ function setup_programs(){
     # https://doc.eresearch.unige.ch/hpc/applications_and_libraries
     # module load GCC # ROOT HANDLED
 
+    ml load ROOT/6.20.04-Python-3.7.4
+
     echo "Loading ROOT module"
-    ml load GCC/11.2.0
-    ml load Automake
-    ml load libtool
-    ml load OpenMPI/4.1.1
-    module load ROOT
+    # ml load GCC/11.2.0
+    # ml load Automake
+    # ml load libtool
+    # ml load OpenMPI/4.1.1
+    # module load ROOT
 
     echo "Loading yaml-cpp module"
-    ml load GCCcore/11.3.0
-    module load yaml-cpp/0.7.0
+    # ml load GCCcore/11.3.0
+    # module load yaml-cpp/0.7.0
 
     echo "Loading other modules..."
-    module load nlohmann_json
+    # module load nlohmann_json
 
-    module load Python
-    module load CMake
-    module load git
-    module load CUDA
-    ml load Geant4
+    # module load Python
+    # module load CMake
+    # module load git
+    # module load CUDA
+    # ml load Geant4
 
     export OA_INPUT_FOLDER="${HOME}/work/inputs/gundam"
   elif [[ $machineName =~ .cern.ch$ ]];
