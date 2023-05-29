54cc76bc6fa4d9b9b7e90d3f26470d457d32ff2b
Adrien Blanchet <adrien.blanchet@live.fr>
2023-05-29 13:44:57 +0200

    Fast commit.

--- t2k_setup.sh
+++ t2k_setup.sh
@@ -186,8 +186,7 @@ function link_t2k_soft()
     source /cvmfs/sft.cern.ch/lcg/contrib/cuda/12.1/x86_64-centos7/setup.sh
 
     echo -e "${WARNING} Initializing Python 3"
-    export PATH="/cvmfs/sft.cern.ch/lcg/releases/Python/3.9.12-9a1bc/x86_64-centos7-gcc11-opt/bin:$PATH"
-    export LD_LIBRARY_PATH="/cvmfs/sft.cern.ch/lcg/releases/Python/3.9.12-9a1bc/x86_64-centos7-gcc11-opt/lib:$LD_LIBRARY_PATH"
+    source /cvmfs/sft.cern.ch/lcg/releases/Python/3.9.12-9a1bc/x86_64-centos7-gcc11-opt/Python-env.sh
 
     echo -e "${WARNING} Initializing clang"
     export PATH="/cvmfs/sft.cern.ch/lcg/releases/clang/15.0.7-27d6b/x86_64-centos7-gcc11-opt/bin:$PATH"
