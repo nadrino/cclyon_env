b9c815c8359a94d269512d8988901f0fd588cb75
Adrien Blanchet <adrien.blanchet@live.fr>
2023-05-29 14:03:13 +0200

    Fast commit.

--- t2k_setup.sh
+++ t2k_setup.sh
@@ -186,8 +186,8 @@ function link_t2k_soft()
     echo -e "${WARNING} Initializing ROOT 6.28.00"
     source /cvmfs/sft.cern.ch/lcg/latest/ROOT/6.28.00-fd53a/x86_64-ubuntu2204-gcc11-opt/bin/thisroot.sh
 
-    echo -e "${WARNING} Initializing ZLib 1.2.11"
-    source /cvmfs/sft.cern.ch/lcg/releases/zlib/1.2.11-8af4c/x86_64-centos7-gcc11-opt/zlib-env.sh
+    # echo -e "${WARNING} Initializing ZLib 1.2.11"
+    # source /cvmfs/sft.cern.ch/lcg/releases/zlib/1.2.11-8af4c/x86_64-centos7-gcc11-opt/zlib-env.sh
 
     echo -e "${WARNING} Initializing LZ4 1.9.2"
     source /cvmfs/sft.cern.ch/lcg/releases/lz4/1.9.2-9bdfe/x86_64-centos7-gcc11-opt/lz4-env.sh
